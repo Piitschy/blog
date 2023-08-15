c36dfc21f4298f65f0a49403d1f7764427ff70f2
Jan <55839828+Piitschy@users.noreply.github.com>
2023-08-16 00:07:42 +0200

    init

---
+++ astro/src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Jans Blog';
+export const SITE_DESCRIPTION = 'Willkommen auf meinem Blog.\nHier schreibe ich über über kleine Projekte, die sich so im Alltag ergeben.';
